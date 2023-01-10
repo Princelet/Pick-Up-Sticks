dc17fe9d065f08d7d5562a358518d26fb8886b7c
SAM STEWART <tenshipixel@gmail.com>
2023-01-10 15:58:41 +0000

    Screen Styles

--- PickUpSticksVS/Main.cpp
+++ PickUpSticksVS/Main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::Titlebar | sf::Style::None);
+    sf::CircleShape shape(150.f);
+    shape.setFillColor(sf::Color(190, 211, 91));
 
     while (window.isOpen())
     {
